9a94488ea990dbbb273fd8954f5c66434f2680e4
Jeiel <github.esvlc@slmail.me>
2024-01-19 12:14:21 -0300

    Create api.py

---
+++ api.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, jsonify, redirect
+
+app = Flask(__name__)
+
+# Dicionário como fonte de dados
+dados = {
+    'titulo': 'Playlist',
+    'conteudo': 'Os links foram encontrados na internet de forma pública!',
+    'informacoes': {
+        'link1': 'https://superflixapi.top/',
+        'link2': 'https://warezstream.net/',
+        'link3': 'https://embedder.net/',
+        'link4': 'https://warezcdn.com/',
+    }
+}
+
+# Rota padrão '/'
+@app.route('/')
+def home():
+    return render_template('home.html', dados=dados)
+
+# Rota '/api' para retornar os dados em formato JSON
+@app.route('/api')
+def api():
+    return jsonify(dados)
+
+# Redireciona todas as outras rotas para '/'
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
